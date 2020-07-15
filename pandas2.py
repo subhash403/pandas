d5f1203df38c613287e2192465484db1b37cd906
subhash403 <rathnasubhash3@gmail.com>
2020-07-15 23:05:34 +0530

    Pandas series and DataFrame Explanation

---
+++ pandas2.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+# For Print the range of dates by using frequencies
+a = pd.date_range('20200615', periods=10, freq='D')
+print(a)
+
+# np random
+# For getting random data 10 columns and 4 rows
+r = np.random.randn(10, 4)
+print(r)
+
+df = pd.DataFrame(r)
+print(df)
+
+# Use date_range as index
+
+df = pd.DataFrame(r, index=a, columns=['A', 'B', 'C', 'D'])
+print(df)
+
+# For CHecking the colmns and shape
+print(df.shape)
+print(df.columns)
+print(df.info())
+
+# By default head or tail as 5
+print(df.head(1))
+print(df.tail())
+
+# For Checking the Present Nul Values
+print(df.isnull())
+
+# For Count the null Values by Column
+print(df.isnull().sum())
+
+# For Drop the None/na Values by using dropna
+print(df.dropna())
+
+# If you want to drop the Entire Column you can use df.dropna(axis=1) then it will delete the entire column
+#df.value_counts()
+print(df.corr())
